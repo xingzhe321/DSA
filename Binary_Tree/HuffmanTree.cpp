01cbc15f30cd0091952780b7e46081b172e13a52
xingzhe <zhe.xing@hnu.edu.cn>
2022-05-20 12:34:33 +0800

    update HuffmanCode

--- Binary_Tree/HuffmanTree.cpp
+++ Binary_Tree/HuffmanTree.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 //采用顺序存储结构————一维数组结构
 //结点类型定义
@@ -64,13 +65,63 @@ void ReadHuffman(const HuffmanTree &HT, const int &n) {
 	cout << endl;
 }
 
-// n = 8, w = {5, 29, 7, 8, 14, 23, 3, 11}
+//哈夫曼树测试函数 案例：n = 8, w = {5, 29, 7, 8, 14, 23, 3, 11}
 void test() {
 	HTNode* HT;
 	int n = 8;
 	CreatHuffmanTree(HT, n);
 	ReadHuffman (HT, 2 * n - 1);
 	delete []HT;//手动释放空间
+}
+
+//哈夫曼编码
+//哈夫曼编码定义
+typedef char **HuffmanCode;
+
+//哈夫曼编码算法实现
+void CreatHuffmanCode(const HuffmanTree &HT, HuffmanCode &HC, const int &n){//从叶子到跟逆求每个字符的哈夫曼编码，存储在编码表HC中
+	HC = new char *[n + 1];//0位不用，从1号位开始
+	char *cd = new char[n];//使用0位（但不一定用到，从后往前存放），临时存放每个字符编码的动态数组空间
+	cd[n - 1] = '\0';//最后一位放置编码结束符
+	for (int i = 1; i <= n; i++) {
+		int start = n - 1;//start开始指向最后
+		int c = i, f = HT[i].parent;//f指向结点c的双亲结点
+		while (f != 0) {
+			start--;//每次回溯start向前移动一位
+			if (HT[f].lch == c) cd[start] = '0';
+			else cd[start] = '1';
+			c = f;
+			f = HT[f].parent;//继续向上回溯			
+		}//while结束，求出第i个字符的编码
+		HC[i] = new char[n - start];//为第i个字符编码分配空间
+		//strcpy函数把含有'\0'结束符的字符串复制到另一个地址空间
+		strcpy(HC[i], &cd[start]);//形参都是字符串的开始地址
+	}//for
+	delete []cd;//释放临时空间
+}
+//用栈实现 后进先出
+void StackHuffmanCode(const HuffmanTree &HT, HuffmanCode &HC, const int &n){
+	HC = new char *[n + 1];
+	//用栈保存临时数组
+	stack<char> cd;
+	cd.push('\0');
+	for (int i = 1; i <= n; i++) {
+		int c = i, f = HT[i].parent;
+		while (f != 0)
+		{
+			if (HT[f].lch == c) cd.push('0');
+			else cd.push('1');
+			c = f;
+			f = HT[f].parent;
+		}
+		HC[i] = new char[cd.size()];
+		char ch;
+		int ind = 0;
+		while (ch = cd.top())
+		{
+			HC[i][ind++] = ch;
+		}
+	}
 }
 
 int main() {
